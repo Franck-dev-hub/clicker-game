9ad0c11099593e088ee5dd843b18e7c03baeaef7
Franck S. <franck.spadotto@yahoo.com>
2026-03-28 12:53:25 +0100

    feat: init logic

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,43 @@
-<script setup>
+<script>
+// Jardinier galactique
+// Ambiance zen, lofi
+// Fait pousser de la mousse / herbe / felurs / arbres ...
+// Change la couleur en fonction du niveau d'oxygene
+
+export default {
+  // Properties exposed on 'this'
+  data() {
+    return {
+      money: 0,
+      clickValue: 1,
+      autoClicker: 0
+    }
+  },
+
+  // Function used continuously
+  methods: {
+    updateMoney() {
+      this.money += this.clickValue;
+    },
+    updateValue() {
+      this.clickValue++;
+    }
+  },
+
+  // Function use at start
+  mounted() {
+    console.log(`The initial count is ${this.money} flouz and ${this.clickValue} click value.`)
+  }
+}
 </script>
 
 <template>
+  <button @click="updateMoney">Money {{ money }}</button> flouz<br>
+  <button @click="updateValue">Click value {{ clickValue }}</button> / click
 </template>
 
 <style scoped>
+button {
+  font-weight: bold;
+}
 </style>
